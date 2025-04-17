d5b6ad536360d971eacb541fc9d55c88c1e67b4d
overclocked-2124 <sumukhaupadhyaya@gmail.com>
2025-04-17 20:37:26 +0530

    solved problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    newArr=[];
+    count=0;
+    for(i=0;i<arr.length;i++){
+        if(args.includes(arr[i])){
+            continue;
+        }
+        newArr[count]=arr[i]
+        count++
+    }
+    return(newArr)
 };
 
 // Do not edit below this line
