6e8963ed1851604c441e8bae0ca0cdfc143e2cbd
overclocked-2124 <sumukhaupadhyaya@gmail.com>
2025-04-18 08:08:41 +0530

    solved problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum,secondNum) {
+    sum=0
+while(secondNum>=firstNum){
+    sum+=secondNum;
+    secondNum--;
+}
+return(sum)
 };
 
 // Do not edit below this line
